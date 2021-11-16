bec564dc9e7e277b72167c58651a04d061ef638f
Rodriques <rodriques.perry@gmail.com>
2021-11-16 15:37:15 -0600

    complete: example sql script

---
+++ exampleScript.sql
@@ -0,0 +1,9 @@
+USE tables_lecture_db;
+
+CREATE TABLE IF NOT EXISTS quotes (
+                        id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                        author_first_name VARCHAR(50) DEFAULT 'NONE',
+                        author_last_name  VARCHAR(100) NOT NULL,
+                        content TEXT NOT NULL,
+                        PRIMARY KEY (id)
+);
