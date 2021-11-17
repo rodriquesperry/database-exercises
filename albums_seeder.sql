1af85e29d2dd20bbcf14fb171e348003a0e84bd8
Rodriques <rodriques.perry@gmail.com>
2021-11-17 11:46:48 -0600

    complete: Basic Statements Exercises seeder script through 3.a write your queries as separate INSERT statements for each record and test. You should see no output.

---
+++ albums_seeder.sql
@@ -0,0 +1,12 @@
+USE codeup_test_db;
+
+INSERT INTO albums (artist, name, release_date, genre, sales)
+VALUES ('Michael Jackson', 'Thriller', 1982, 'Pop', 48.9);
+INSERT INTO albums (artist, name, release_date, genre, sales)
+VALUES ('AC/DC', 'Back in Black', 1980, 'Hard Rock', 29.6);
+INSERT INTO albums (artist, name, release_date, genre, sales)
+VALUES ('Whitney Houston', 'The Bodyguard', 1992, 'R&B, Soundtrack', 32.4);
+INSERT INTO albums (artist, name, release_date, genre, sales)
+VALUES ('Eagles', 'Their Greatest Hits (1971-1975)', 1976, 'Country, Rock', 41.2);
+INSERT INTO albums (artist, name, release_date, genre, sales)
+VALUES ('Eagles', 'Hotel California', 1976, 'Progressive Rock', 31.5);
