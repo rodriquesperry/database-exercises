1e62694161746c7aa6934dfa7543070d231891b5
Rodriques <rodriques.perry@gmail.com>
2021-11-17 09:54:13 -0600

    refactor: changed release date from CHAR to INT

--- albums_migration.sql
+++ albums_migration.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(50) NOT NULL DEFAULT 'NONE',
     name VARCHAR(50) NOT NULL,
-    release_date  CHAR(4) NOT NULL,
+    release_date  INT NOT NULL,
     sales FLOAT NOT NULL,
     genre VARCHAR(20),
     PRIMARY KEY (id)
