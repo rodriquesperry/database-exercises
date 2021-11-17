ff41cc2fec3f8d2dcd58f5a8dbf60f32b352a31c
Rodriques <rodriques.perry@gmail.com>
2021-11-17 12:32:21 -0600

    complete: Basic Statements Exercises seeder script through 3.b - Refactor your script to use a single INSERT statement for all the records and test it again. Again, this should not generate any output.

--- albums_migration.sql
+++ albums_migration.sql
@@ -5,9 +5,9 @@ DROP TABLE IF EXISTS albums;
 CREATE TABLE IF NOT EXISTS albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(50) NOT NULL DEFAULT 'NONE',
-    name VARCHAR(50) NOT NULL,
+    name VARCHAR(100) NOT NULL,
     release_date  INT NOT NULL,
     sales FLOAT NOT NULL,
-    genre VARCHAR(20),
+    genre VARCHAR(50),
     PRIMARY KEY (id)
 );
--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,12 +1,36 @@
 USE codeup_test_db;
 
 INSERT INTO albums (artist, name, release_date, genre, sales)
-VALUES ('Michael Jackson', 'Thriller', 1982, 'Pop', 48.9);
-INSERT INTO albums (artist, name, release_date, genre, sales)
-VALUES ('AC/DC', 'Back in Black', 1980, 'Hard Rock', 29.6);
-INSERT INTO albums (artist, name, release_date, genre, sales)
-VALUES ('Whitney Houston', 'The Bodyguard', 1992, 'R&B, Soundtrack', 32.4);
-INSERT INTO albums (artist, name, release_date, genre, sales)
-VALUES ('Eagles', 'Their Greatest Hits (1971-1975)', 1976, 'Country, Rock', 41.2);
-INSERT INTO albums (artist, name, release_date, genre, sales)
-VALUES ('Eagles', 'Hotel California', 1976, 'Progressive Rock', 31.5);
+VALUES  ('Michael Jackson', 'Thriller', 1982, 'Pop', 48.9),
+        ('AC/DC', 'Back in Black', 1980, 'Hard Rock', 29.6),
+        ('Whitney Houston', 'The Bodyguard', 1992, 'R&B, Soundtrack', 32.4),
+        ('Meat Loaf', 'Bat Out of Hell', 1977, 'Hard Rock, Glam rock', 21.7),
+        ('Eagles', 'Their Greatest Hits (1971-1975)', 1976, 'Country, Rock', 41.2),
+        ('Pink Floyd', 'The Dark Side of the Moon', 1973, 'Progressive Rock', 24.4),
+        ('Eagles', 'Hotel California', 1976, 'Progressive Rock', 31.5),
+        ('Bee Gees', 'Saturday Night Fever', 1977, 'Disco', 21.6),
+        ('Fleetwood Mac', 'Rumours', 1977, 'Soft Rock', 27.9),
+        ('Shania Twain', 'Come On Over', 1997, 'Country, Pop', 29.6),
+        ('Various Artists', 'Grease: The Original Soundtrack', 1978, 'Rock and roll', 14.4),
+        ('Led Zeppelin', 'Led Zeppelin IV', 1971, 'Hard Rock, Heavy Metal', 29.0),
+        ('Michael Jackson', 'Bad', 1987, 'Pop, Rock', 20.7),
+        ('Alanis Morissette', 'Jagged Little Pill', 1995, 'Alternative Rock', 24.3),
+        ('Michael Jackson', 'Dangerous', 1991, 'New Jack Swing, Pop', 17.0),
+        ('Celine Dion', 'Falling into You', 1996, 'Pop, Soft Rock', 21.1),
+        ('The Beatles', 'Sgt. Peppers Lonely Hearts Club Band', 1967, 'Rock', 18.2),
+        ('Various Artists', 'Dirty Dancing', 1987, 'Pop, rock, R&B', 17.9),
+        ('Adele', '21', 2011, 'Pop, Soul', 26.4),
+        ('Celine Dion', 'Let''s Talk About Love', 1997, 'Pop, Soft Rock', 19.3),
+        ('Metallica', 'Metallica', 1991, 'Heavy Metal', 25.2),
+        ('The Beatles', '1', 2000, 'Rock', 23.5),
+        ('The Beatles', 'Abbey Road', 1969, 'Rock', 14.4),
+        ('ABBA', 'Gold: Greatest Hits', 1992, 'Pop, Disco', 23.0),
+        ('Bruce Springsteen', 'Born in the U.S.A.', 1984, 'Heartland Rock', 19.6),
+        ('Madonna',	'The Immaculate Collection', 1990, 'Pop, Dance', 19.5),
+        ('Pink Floyd', 'The Wall', 1979, 'Progressive Rock', 18.7),
+        ('James Horner', 'Titanic: Music from the Motion Picture', 1997, 'Film Score', 18.1),
+        ('Dire Straits', 'Brothers in Arms', 1985, 'Roots Rock, Blues', 17.7),
+        ('Nirvana', 'Nevermind', 1991, 'Grunge, Alternative Rock', 16.7),
+        ('Santana', 'Supernatural', 1999, 'Latin rock', 20.5),
+        ('Guns N'' Roses', 'Appetite for Destruction', 1987, 'Hard Rock', 21.9),
+        ('Elton John', 'Goodbye Yellow Brick Road', 1973, 'Rock, Pop, Glam', 8.5);
