8189df0fde93620f51fefcb13d3689fbefc2b5dc
Rodriques <rodriques.perry@gmail.com>
2021-11-18 14:52:05 -0600

    complete WHERE Clause PART TWO

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,7 +1,13 @@
 USE employees;
 
-SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+SELECT * FROM employees WHERE gender = 'M' AND (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya');
 
 SELECT * FROM employees WHERE last_name LIKE 'e%';
 
 SELECT * FROM employees WHERE last_name LIKE '%q%';
+
+SELECT * FROM employees WHERE last_name LIKE 'e%' OR last_name LIKE '%e';
+
+SELECT * FROM employees WHERE last_name LIKE 'e%e';
+
+SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
