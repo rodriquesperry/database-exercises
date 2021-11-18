d82a30b19c7e83b1b1a274d156c5d14320272910
Rodriques <rodriques.perry@gmail.com>
2021-11-18 15:24:47 -0600

    complete: Order By exercise through number 2

---
+++ order_by_exercises.sql
@@ -0,0 +1,13 @@
+USE employees;
+
+SELECT * FROM employees WHERE gender = 'M' AND (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') ORDER BY first_name;
+
+SELECT * FROM employees WHERE last_name LIKE 'e%';
+
+SELECT * FROM employees WHERE last_name LIKE '%q%';
+
+SELECT * FROM employees WHERE last_name LIKE 'e%' OR last_name LIKE '%e';
+
+SELECT * FROM employees WHERE last_name LIKE 'e%e';
+
+SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
