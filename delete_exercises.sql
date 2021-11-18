d3bd1744b3c9d54e8e4d28d56a4e5810da036eda
Rodriques <rodriques.perry@gmail.com>
2021-11-18 10:14:11 -0600

    complete: delete_exercises.sql through number 7

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,11 +1,11 @@
 USE codeup_test_db;
 
 SELECT 'All albums released after to 1991' AS 'INFO';
-DELETE * From albums WHERE release_date > 1991;
+DELETE FROM albums WHERE release_date > 1991;
 
 SELECT 'All the albums with a genre of "Disco"' AS 'INFO';
-DELETE * FROM albums WHERE genre = 'Disco';
+DELETE FROM albums WHERE genre = 'Disco';
 
 SELECT 'The name of all albums by Whitney Houston' AS 'INFO';
-DELETE * FROM albums WHERE artist = 'Whitney Houston';
+DELETE FROM albums WHERE artist = 'Whitney Houston';
 
