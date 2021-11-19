69a5509e8c49927e13cfb38ef461de3224c5f5be
Rodriques <rodriques.perry@gmail.com>
2021-11-19 10:26:21 -0600

    complete: Functions exercises through number 4

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -34,6 +34,13 @@ WHERE last_name LIKE '%q%'
 AND last_name
 NOT LIKE '%qu%';
 
-SELECT * FROM employees
+SELECT *
+FROM employees
 WHERE month(birth_date) = 12
+AND day(birth_date) = 25;
+
+SELECT *
+FROM employees
+WHERE year(hire_date) BETWEEN 1990 AND 1999
+AND month(birth_date) = 12
 AND day(birth_date) = 25;
