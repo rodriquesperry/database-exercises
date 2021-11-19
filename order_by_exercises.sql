265fd3bc6712624e0f47622e2dd2d98a399f898f
Rodriques <rodriques.perry@gmail.com>
2021-11-19 09:30:39 -0600

    refactor: update sql files

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,13 +1,33 @@
 USE employees;
 
-SELECT * FROM employees WHERE gender = 'M' AND (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') ORDER BY last_name DESC, first_name;
+SELECT *
+FROM employees
+WHERE gender = 'M'
+AND (first_name = 'Irena'
+OR first_name = 'Vidya'
+OR first_name = 'Maya')
+ORDER BY last_name DESC, first_name;
 
-SELECT * FROM employees WHERE last_name LIKE 'e%';
+SELECT *
+FROM employees
+WHERE last_name LIKE 'e%';
 
-SELECT * FROM employees WHERE last_name LIKE '%q%';
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%';
 
-SELECT * FROM employees WHERE last_name LIKE 'e%' OR last_name LIKE '%e' ORDER BY emp_no DESC;
+SELECT *
+FROM employees
+WHERE last_name LIKE 'e%'
+OR last_name LIKE '%e'
+ORDER BY emp_no DESC;
 
-SELECT * FROM employees WHERE last_name LIKE 'e%e';
+SELECT *
+FROM employees
+WHERE last_name LIKE 'e%e';
 
-SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%'
+AND last_name
+NOT LIKE '%qu%';
