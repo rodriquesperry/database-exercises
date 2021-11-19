70b2a14ddba0ddb287a31fa0f4ed8a4a02baf2ef
Rodriques <rodriques.perry@gmail.com>
2021-11-19 10:34:46 -0600

    complete: Functions exercises through number 6

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -51,3 +51,9 @@ WHERE year(hire_date) BETWEEN 1990 AND 1999
 AND month(birth_date) = 12
 AND day(birth_date) = 25
 ORDER BY hire_date DESC;
+
+SELECT datediff(now(), hire_date)
+FROM employees
+WHERE year(hire_date) BETWEEN 1990 AND 1999
+AND month(birth_date) = 12
+AND day(birth_date) = 25;
