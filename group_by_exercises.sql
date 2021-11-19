40f4bf89ed2d74717b6558fc3cb902a3c0c5729a
Rodriques <rodriques.perry@gmail.com>
2021-11-19 14:53:59 -0600

    refactored: GROUP BY exercises

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -2,12 +2,14 @@ USE employees;
 
 SELECT DISTINCT title FROM titles;
 
-SELECT last_name
+SELECT first_name, last_name
 FROM employees
 WHERE last_name LIKE 'e%e'
-GROUP BY last_name, first_name;
+GROUP BY last_name, first_name
+ORDER BY last_name;
 
 SELECT COUNT(first_name), last_name
+AS 'Number of People Who Have These Last Names'
 FROM employees
 WHERE last_name LIKE '%q%'
     AND last_name NOT LIKE '%qu%'
@@ -15,6 +17,7 @@ GROUP BY last_name
 ORDER BY last_name;
 
 SELECT COUNT(*), gender
+AS 'Number of Males and Females Who Have These First Names'
 FROM employees
 WHERE (first_name = 'Irena'
     OR first_name = 'Vidya'
