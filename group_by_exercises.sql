c35a2a6f2ecdff06a75603ba6aa401fc145db71e
Rodriques <rodriques.perry@gmail.com>
2021-11-19 12:22:47 -0600

    complete: GROUP BY exercises up-to number 5

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -6,3 +6,9 @@ SELECT last_name
 FROM employees
 WHERE last_name LIKE 'e%e'
 GROUP BY last_name, first_name;
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
