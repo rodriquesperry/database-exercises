490ee72ba65933d19800d4648b86d7dfcc01dbf6
Rodriques <rodriques.perry@gmail.com>
2021-11-19 12:28:56 -0600

    complete: GROUP BY exercises up-to number 7

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -10,6 +10,13 @@ GROUP BY last_name, first_name;
 SELECT COUNT(first_name), last_name
 FROM employees
 WHERE last_name LIKE '%q%'
-AND last_name NOT LIKE '%qu%'
+    AND last_name NOT LIKE '%qu%'
 GROUP BY last_name
 ORDER BY last_name;
+
+SELECT COUNT(*), gender
+FROM employees
+WHERE (first_name = 'Irena'
+    OR first_name = 'Vidya'
+    OR first_name = 'Maya')
+GROUP BY gender;
