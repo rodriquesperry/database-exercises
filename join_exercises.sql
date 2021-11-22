01bf392909d146a636b419f6208530a672cfbd5e
Rodriques <rodriques.perry@gmail.com>
2021-11-22 14:49:34 -0600

    complete: Find the name of all departments currently managed by women.

--- join_exercises.sql
+++ join_exercises.sql
@@ -8,3 +8,12 @@ JOIN departments as d
 ON d.dept_no = de.dept_no
 WHERE de.to_date = '9999-01-01'
 ORDER BY dept_name;
+
+SELECT d.dept_name, CONCAT(e.first_name, ' ', e.last_name) AS full_name
+FROM employees as e
+         JOIN dept_manager as de
+              ON de.emp_no = e.emp_no
+         JOIN departments as d
+              ON d.dept_no = de.dept_no
+WHERE de.to_date = '9999-01-01' AND e.gender = 'F'
+ORDER BY dept_name;
