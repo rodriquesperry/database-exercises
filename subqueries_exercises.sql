7e6e53ec871744db3ee3138bb535119a575d6c0c
Rodriques <rodriques.perry@gmail.com>
2021-11-23 11:49:17 -0600

    Sub-Queries Bonus #1

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -24,3 +24,16 @@ WHERE emp_no IN (
     FROM dept_manager
     WHERE gender = 'F' AND to_date > NOW()
     );
+
+SELECT dept_name
+FROM departments
+WHERE dept_no IN (
+    SELECT dept_no
+    FROM dept_manager
+    WHERE emp_no IN (
+        SELECT emp_no
+        FROM employees
+        WHERE gender = 'F'
+        AND to_date > NOW()
+        )
+)
